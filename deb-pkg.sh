5ab9265624d04c91addfccd1aa0786315ee32ffd
Your Name <you@example.com>
2023-01-08 07:46:47 +0300

    Fix lintian errors

--- deb-pkg.sh
+++ deb-pkg.sh
@@ -5,7 +5,9 @@ ARCH_NAME="${PKG_NAME}-${VERSION}.tar.gz"
 
 debmake -y -p ${PKG_NAME} \
     -u ${VERSION} -r 1 -x1 -n \
-    -e "iliya.gorshkov@yandex.ru" \
+    -e "igorshkov@k-tech.ru" \
     -f "Iliya Gorshkov" \
     -b':bin' \
     -i debuild
+
+lintian ../${PKG_NAME}_${VERSION}_amd64.deb
